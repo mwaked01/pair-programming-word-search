46155b744cd6cb01c12f636ebb6de5ba895514a2
Mohamed Waked <mwaked01@gmail.com>
2023-08-19 17:18:14 -0400

    implement wordSearch to check id the words exists in the letters matrix

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,61 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+  // vertical word
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['L', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['G', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['T', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LIGHT');
+
+    assert.isTrue(result);
+  });
+  
+  // empty matrix
+  it("should return false if the matrix is empty", function() {
+    const result = wordSearch([],'');
+    assert.isFalse(result);
+  });
+
+  // empty search
+  it("should return false if the search is empty", function() {
+    const result = wordSearch([
+      ['L', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['G', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['T', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L']],' ');
+    assert.isFalse(result);
+  });
+
+  // lowercase search
+  it("should return false if the search is lowercase", function() {
+    const result = wordSearch([
+      ['L', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['G', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['T', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L']],'light');
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,47 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  console.log(letters);
+  // checks for empty array
+  if (letters.length === 0) {
+    return false;
+  }
+  //
+  if (horizontalSearch(letters, word)) {
+    return true;
+  // call transpose to flip matrix then run horizontal search again
+  } else if (horizontalSearch(transpose(letters), word)) {
+    return true;
+  }
 
-module.exports = wordSearch
+  return false;
+};
+// input 2d matrix of letters and word to search
+// return whether word exists horizontally
+const horizontalSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+};
+
+// flips 2d array along x/y axis
+const transpose = (letters) => {
+  let flipped = [];
+  for (let x = 0; x < letters[0].length; x++) {
+    flipped.push([]);
+    for (let y = 0; y < letters.length; y++) {
+      flipped[x].push(letters[y][x]);
+    }
+  }
+  return flipped;
+};
+
+//console.log(wordSearch([],' '));
+
+module.exports = wordSearch;
+
+
+
+
+
+
+
